6c1250af268fd5167b2e4264d42d754f1d6bcaf0
uli <jakub.uli@gmail.com>
2020-05-02 16:54:48 +0200

    basic calculator app

---
+++ sololearn/BasicCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BasicCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int a, b, result;
+            
+            do{
+            Console.Write("a = ");
+            input = Console.ReadLine();
+            if(input == "exit")
+                break;
+            a = Convert.ToInt32(input);
+
+            Console.Write("b = ");
+            b = Convert.ToInt32(Console.ReadLine());
+            
+            result = a + b;
+            Console.WriteLine($"result = {result}");
+            }while(true);
+        }
+    }
+}
