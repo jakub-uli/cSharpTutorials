ba5aa3d30e3dd5ff5ee30579e4b5de03c39372ea
uli <jakub.uli@gmail.com>
2020-05-10 23:39:59 +0200

    First commit

---
+++ sololearn/Pyramid/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pyramid
+{
+    class Program
+    {
+        static void DrawPyramid(int levels)
+        {
+            for(int i = 0; i < levels; i++)
+            {
+                for(int j = 1; j < levels - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int k = 0; k < i * 2 - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            DrawPyramid(6);
+        }
+    }
+}
